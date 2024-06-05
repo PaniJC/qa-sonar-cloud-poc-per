524657f4166bf23cd6c4893511d06f4289c2891e
David Steven Romero Toquica <david.steven@valere.io>
2024-06-05 14:45:21 -0500

    chore: Update main.py to configure Datadog tracer

--- app/main.py
+++ app/main.py
@@ -1,10 +1,15 @@
 from fastapi import FastAPI
-from ddtrace import patch
+from ddtrace import patch, tracer
 from routers.routers import routers
 from exceptions.exception_handler import add_exception_handlers
 
 
 patch(fastapi=True)
+
+tracer.configure(
+    hostname="datadog-agent",
+    port=8126,
+)
 
 app = FastAPI()
 
