8401bbbb29b0e7ae6db6075033959f1bc57cb5e5
David Steven Romero Toquica <david.steven@valere.io>
2024-06-05 13:08:28 -0500

    chore: Update Dockerfile to use Python 3.11 and AWS deployment scripts and Docker configuration

--- CICD/codedeploy-scripts/set-permissions.sh
+++ CICD/codedeploy-scripts/set-permissions.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 source /home/ec2-user/.profile
-chown -R ec2-user:ec2-user /home/ec2-user/poc-ec2-deploy
+chown -R ec2-user:ec2-user /home/ec2-user/deploy
--- CICD/docker-scripts/pull.sh
+++ CICD/docker-scripts/pull.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sudo echo pull new image
-sudo aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 590183966112.dkr.ecr.us-east-1.amazonaws.com/poc-ec2-deploy
-docker pull 590183966112.dkr.ecr.us-east-1.amazonaws.com/poc-ec2-deploy:Lasted
+sudo aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 590183966112.dkr.ecr.us-east-1.amazonaws.com/onboarding-backend
+docker pull 590183966112.dkr.ecr.us-east-1.amazonaws.com/onboarding-backend:Lasted
 chmod +x ./docker-scripts/startup.sh
