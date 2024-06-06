1ea23f812455953985083a8e614f33ed4df3aa77
David Steven Romero Toquica <david.steven@valere.io>
2024-06-06 10:04:04 -0500

    chore: Enable runtime metrics with Datadog in main.py and docker-compose.yml

--- app/main.py
+++ app/main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
 from ddtrace.runtime import RuntimeMetrics
+from ddtrace import patch_all
 from routers.routers import routers
 from exceptions.exception_handler import add_exception_handlers
 
+patch_all()
 RuntimeMetrics.enable()
 
 app = FastAPI()
